f5fc087c3aba528bdb67b6d3fa52b0b4c5039d32
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-07-05 20:51:15 -0500

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,83 @@ import { useState } from 'react'
 import './App.css'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import RootLayout from '@/layout/RootLayout';
+import { useToast } from "@/components/ui/use-toast"
+import { ToastAction } from './components/ui/toast';
 
 function App() {
   const [count, setCount] = useState(0)
+  const { toast } = useToast()
 
   return (
     <>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
+      <RootLayout>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
 
-      <Button variant={'secondary'}>Secondary</Button>
+        <Button variant={'secondary'}>Secondary</Button>
 
-      <Button asChild variant={'link'}>
-        <a href="#">Link</a>
-      </Button>
+        <Button asChild variant={'link'}>
+          <a href="#">Link</a>
+        </Button>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="name" className="text-right">
-                Name
-              </label>
-              <input
-                type='text'
-                id="name"
-                defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="name" className="text-right">
+                  Name
+                </label>
+                <input
+                  type='text'
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="username" className="text-right">
+                  Username
+                </label>
+                <input
+                  type='password'
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="username" className="text-right">
-                Username
-              </label>
-              <input
-                type='password'
-                id="username"
-                defaultValue="@peduarte"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up ",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+              action: (
+                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+              ),
+            })
+          }}
+        >
+          Add to calendar
+        </Button>
+      </RootLayout>
     </>
   )
 }
