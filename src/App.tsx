57e84783ce27eee260da839ee93a904e51e0941a
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-09-19 01:25:27 -0500

    added accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import RootLayout from '@/layout/RootLayout';
-import { useToast } from "@/components/ui/use-toast"
-import { ToastAction } from './components/ui/toast';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import RootLayout from '@/layout/RootLayout'
+import { useToast } from '@/components/ui/use-toast'
+import { ToastAction } from './components/ui/toast'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -40,7 +54,7 @@ function App() {
                   Name
                 </label>
                 <input
-                  type='text'
+                  type="text"
                   id="name"
                   defaultValue="Pedro Duarte"
                   className="col-span-3"
@@ -51,7 +65,7 @@ function App() {
                   Username
                 </label>
                 <input
-                  type='password'
+                  type="password"
                   id="username"
                   defaultValue="@peduarte"
                   className="col-span-3"
@@ -68,8 +82,8 @@ function App() {
           variant="outline"
           onClick={() => {
             toast({
-              title: "Scheduled: Catch up ",
-              description: "Friday, February 10, 2023 at 5:57 PM",
+              title: 'Scheduled: Catch up ',
+              description: 'Friday, February 10, 2023 at 5:57 PM',
               action: (
                 <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
               ),
@@ -78,6 +92,30 @@ function App() {
         >
           Add to calendar
         </Button>
+
+        <hr />
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </RootLayout>
     </>
   )
